62c68bad6fa3d9d6a0dde34ca1e6db09e2936364
abro5723 <abro5723@740s-pl-114.local>
2015-09-24 09:35:05 -0600

    Fixing code to work with user error

--- src/input/controller/InputController.java
+++ src/input/controller/InputController.java
@@ -5,15 +5,68 @@ import input.view.PopupDisplay;
 public class InputController
 {
 	private PopupDisplay myPopups;
-	
+
 	public InputController()
 	{
 		myPopups = new PopupDisplay();
 	}
-	
+
 	public void start()
 	{
-		String temp = myPopups.grabAnswer("Type in your name.");
-		myPopups.showResponse("Your name is: " + temp);
+		String myName = myPopups.grabAnswer("Type in your name.");
+		myPopups.showResponse("Your name is: " + myName);
+
+		String temp = myPopups.grabAnswer("Type in your age");
+		int myAge;
+
+		if (isInteger(temp))
+		{
+			myAge = Integer.parseInt(temp);
+		}
+		else
+		{
+			myAge = 9999;
+		}
+		myPopups.showResponse("Your age is: " + myAge);
+
+		String tempWeight = myPopups.grabAnswer("Type in your weight");
+		double myWeight = Double.parseDouble(tempWeight);
+		myPopups.showResponse("Your weight is: " + myWeight);
+
+		myTestThing = new Thingy(myName, myAge, myWeight);
+	}
+
+	private boolean isInteger(String input)
+	{
+		boolean isInt = false;
+
+		try
+		{
+			int temp = Integer.parseInt(input);
+			isInt = true;
+		}
+		catch (NumberFormatException error)
+		{
+			myPopups.showResponse("not an iteger - bad value will be used");
+		}
+
+		return isInt;
+	}
+	
+	private boolean isDouble(String input)
+	{
+		boolean isDouble = false;
+		
+		try
+		{
+			double tempWeight = Double.parseDouble(input);
+			isDouble = true;
+		}
+		catch()
+		{
+			
+		}
+		
+		return isDouble;
 	}
 }
