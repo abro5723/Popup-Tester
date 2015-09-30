ec185f87a438f62d3186f694a133c733efd85ace
abro5723 <abro5723@740s-pl-114.local>
2015-09-30 08:25:46 -0600

    Added more documentation and set defaults for all variables

--- src/input/model/Thingy.java
+++ src/input/model/Thingy.java
@@ -11,14 +11,17 @@ public class Thingy
 	private String name;
 	private int age;
 	private double weight;
-
+	
+	/**
+	 * Sets defaults for all values. They are ridiculous values
+	 */
 	public Thingy()
 	{
 		name = "";
 		age = 99;
 		weight = 534;
 	}
-
+	
 	public Thingy(String name, int age, double weight)
 	{
 		//this. refers to the current instance of an object
@@ -26,20 +29,33 @@ public class Thingy
 		this.age = age;
 		this.weight = weight;
 	}
-
+	
 	public String getName()
 	{
 		return name;
 	}
-
+	
 	public int getAge()
 	{
 		return age;
 	}
-
+	
 	public double getWeight()
 	{
 		return weight;
 	}
-
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
 }
